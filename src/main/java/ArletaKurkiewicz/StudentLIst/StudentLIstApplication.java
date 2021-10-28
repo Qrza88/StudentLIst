faa5ab2a8e6629a4fa08a4cc2a73c6df53cdcfd5
Qrza88 <arleta.kurkiewicz@gmail.com>
2021-10-28 17:54:58 +0200

    Initial commit

---
+++ src/main/java/ArletaKurkiewicz/StudentLIst/StudentLIstApplication.java
@@ -0,0 +1,13 @@
+package ArletaKurkiewicz.StudentLIst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLIstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLIstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ArletaKurkiewicz/StudentLIst/StudentLIstApplicationTests.java
@@ -0,0 +1,13 @@
+package ArletaKurkiewicz.StudentLIst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLIstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
